8d23befabbe7b3e0e6bfe0ad3000d6a9904f7284
zalviandyr <zukronalviandy@gmail.com>
2021-10-19 17:37:02 +0700

    feat: handle same id in download progress

--- lib/models/download_audio.dart
+++ lib/models/download_audio.dart
@@ -1,13 +1,20 @@
-class DownloadAudio {
+import 'package:equatable/equatable.dart';
+
+class DownloadAudio extends Equatable {
+  final String id;
   final String thumb;
   final String title;
   final String duration;
   final String size;
 
-  DownloadAudio({
+  const DownloadAudio({
+    required this.id,
     required this.thumb,
     required this.title,
     required this.duration,
     required this.size,
   });
+
+  @override
+  List<Object?> get props => [id];
 }
